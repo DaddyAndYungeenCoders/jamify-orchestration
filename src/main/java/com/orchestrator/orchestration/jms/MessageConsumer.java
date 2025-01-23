e3e85384599f4da995ba86a6b8ea9ae9d084fcda
SandroSpina698 <sandro.spina.pro@gmail.com>
2025-01-23 16:57:03 +0100

    no jackson here

--- src/main/java/com/orchestrator/orchestration/jms/MessageConsumer.java
+++ src/main/java/com/orchestrator/orchestration/jms/MessageConsumer.java
@@ -1,7 +1,7 @@
 package com.orchestrator.orchestration.jms;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.orchestrator.orchestration.jms.interfaces.MessageConsumers;
+import com.orchestrator.orchestration.objects.vms.PlaylistEndJobConverter;
 import com.orchestrator.orchestration.objects.vms.PlaylistEndJobVM;
 import com.orchestrator.orchestration.services.interfaces.OrchestratorService;
 import jakarta.jms.JMSException;
@@ -11,24 +11,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class MessageConsumer implements MessageConsumers<TextMessage> {
     private final OrchestratorService orchestratorService;
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     @JmsListener(destination = "jamify.orchestrator.playlist-done")
     public void onMessageReceived(TextMessage message) {
         try {
             String json = message.getText();
-            PlaylistEndJobVM playlistEndJobVM = objectMapper.readValue(json, PlaylistEndJobVM.class);
+            PlaylistEndJobVM playlistEndJobVM = PlaylistEndJobConverter.fromJson(json);
             System.out.println("Received playlist: " + playlistEndJobVM);
             orchestratorService.onReceiveMessage(playlistEndJobVM);
-        } catch (JMSException | IOException e) {
+        } catch (JMSException e) {
             e.printStackTrace();
         }
     }
