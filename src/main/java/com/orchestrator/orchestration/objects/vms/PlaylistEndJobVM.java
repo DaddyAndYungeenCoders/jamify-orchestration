8950309f5b06be2875dcdc021e398f814f9124b9
SandroSpina698 <sandro.spina.pro@gmail.com>
2025-01-23 09:55:36 +0100

    change uuid serialization

--- src/main/java/com/orchestrator/orchestration/objects/vms/PlaylistEndJobVM.java
+++ src/main/java/com/orchestrator/orchestration/objects/vms/PlaylistEndJobVM.java
@@ -1,5 +1,7 @@
 package com.orchestrator.orchestration.objects.vms;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.UUID;
@@ -13,4 +15,15 @@ public class PlaylistEndJobVM {
     UUID id;
     String userId;
     PlaylistEndJobPayloadVM data;
+
+    @JsonCreator
+    public PlaylistEndJobVM(
+            @JsonProperty("id") String id,
+            @JsonProperty("userId") String userId,
+            @JsonProperty("data") PlaylistEndJobPayloadVM data
+    ) {
+        this.id = UUID.fromString(id);
+        this.userId = userId;
+        this.data = data;
+    }
 }
