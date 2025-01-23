6c6f8a7cb9d8598bd6197607faf120fb7eb99df3
SandroSpina698 <sandro.spina.pro@gmail.com>
2025-01-23 17:05:32 +0100

    fix - no simple message converter

--- src/main/java/com/orchestrator/orchestration/jms/JmsConfig.java
+++ src/main/java/com/orchestrator/orchestration/jms/JmsConfig.java
@@ -1,14 +1,14 @@
-package com.orchestrator.orchestration.jms;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.SimpleMessageConverter;
-
-@Configuration
-public class JmsConfig {
-    @Bean
-    public MessageConverter jacksonJmsMessageConverter() {
-        return new SimpleMessageConverter();
-    }
-}
+//package com.orchestrator.orchestration.jms;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jms.support.converter.MessageConverter;
+//import org.springframework.jms.support.converter.SimpleMessageConverter;
+//
+//@Configuration
+//public class JmsConfig {
+//    @Bean
+//    public MessageConverter jacksonJmsMessageConverter() {
+//        return new SimpleMessageConverter();
+//    }
+//}
