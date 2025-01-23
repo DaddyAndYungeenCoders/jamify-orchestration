d00e5360a020979973da8d9eac5762ca2b47d85d
SandroSpina698 <sandro.spina.pro@gmail.com>
2025-01-23 16:18:52 +0100

    homard

--- src/main/java/com/orchestrator/orchestration/jms/JmsConfig.java
+++ src/main/java/com/orchestrator/orchestration/jms/JmsConfig.java
@@ -2,17 +2,13 @@ package com.orchestrator.orchestration.jms;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
+import org.springframework.jms.support.converter.SimpleMessageConverter;
 
 @Configuration
 public class JmsConfig {
     @Bean
     public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        return converter;
+        return new SimpleMessageConverter();
     }
 }
